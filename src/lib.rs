9d4c1d23168bfdbc31d9f131033fcbac03d80085
chrismo80 <krizm0@frenet.de>
2023-08-06 10:25:40 +0200

    separate main from aoc lib

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod examples;
+pub mod extensions;
+pub mod year2022;
+
+pub fn run()
+{
+    let times = 1;
+    let start = std::time::Instant::now();
+
+    for _ in 0..times {
+        year2022::day1::solve();
+        year2022::day2::solve();
+    }
+
+    println!("Duration: {:.1} ms", start.elapsed().as_micros() as f32 / 1000.0);
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,8 @@
+use advent_of_code;
+
 #[test]
 fn do_it()
 {
-    println!("Test");
+    advent_of_code::examples::count_items::main();
+    advent_of_code::examples::errors::main();
 }
