0e5d4d8fcd11722ab8a0d35e9ec5a93c97dbf958
chrismo80 <krizm0@frenet.de>
2023-08-06 22:48:55 +0200

    ignore test examples

--- src/examples/tests.rs
+++ src/examples/tests.rs
@@ -15,6 +15,7 @@ mod tests
     use super::*; // import parent module items
 
     #[test]
+    #[ignore]
     fn is_smaller_than()
     {
         for i in 0..10 {
@@ -23,12 +24,14 @@ mod tests
     }
 
     #[test]
+    #[ignore]
     fn is_not_smaller_than()
     {
         assert!(!smaller_than(8, 6));
     }
 
     #[test]
+    #[ignore]
     fn this_test_will_pass()
     {
         assert_eq!(prints_and_returns_10(4), 10);
@@ -36,9 +39,9 @@ mod tests
 
     #[test]
     #[should_panic = "values don't match"]
+    #[ignore]
     fn this_test_will_fail()
     {
-        
         assert_eq!(prints_and_returns_10(8), 5, "values don't match");
     }
 }
