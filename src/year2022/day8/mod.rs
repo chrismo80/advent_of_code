7886f2266556e32f6cc8f98b8ac343f6148578c2
chrismo80 <krizm0@freenet.de>
2023-08-09 15:57:04 +0200

    update day 8

--- src/year2022/day8/mod.rs
+++ src/year2022/day8/mod.rs
@@ -5,30 +5,6 @@ pub fn solve() -> (usize, usize)
         .map(|l| l.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    // let mut result1 = std::sync::Mutex::new(0);
-    // let mut result2 = std::sync::Mutex::new(0);
-
-    // std::thread::scope(|scope| {
-    //     let input = &input; // shadowing to enable move only for x
-    //     let result1 = &result1;
-    //     let result2 = &result2;
-
-    //     for x in 0..input.len() {
-    //         scope.spawn(move || {
-    //             for y in 0..input[0].len() {
-    //                 let mut result1 = result1.lock().unwrap();
-    //                 let mut result2 = result2.lock().unwrap();
-
-    //                 *result1 += visible_from_outside(input, x, y) as usize;
-    //                 *result2 = (*result2).max(scenic_score(input, x, y));
-    //             }
-    //         });
-    //     }
-    // });
-
-    // let result1 = result1.lock().unwrap().to_owned();
-    // let result2 = result2.lock().unwrap().to_owned();
-
     let mut result1 = 0;
     let mut result2 = 0;
 
@@ -46,22 +22,18 @@ pub fn solve() -> (usize, usize)
 
 fn visible_from_outside(forest: &[Vec<char>], x: usize, y: usize) -> bool
 {
-    let visible_t = view_t(forest, x, y).iter().all(|tree| tree < &forest[x][y]);
-    let visible_l = view_l(forest, x, y).iter().all(|tree| tree < &forest[x][y]);
-    let visible_b = view_b(forest, x, y).iter().all(|tree| tree < &forest[x][y]);
-    let visible_r = view_r(forest, x, y).iter().all(|tree| tree < &forest[x][y]);
-
-    visible_t || visible_l || visible_b || visible_r
+    view_t(forest, x, y).iter().all(|tree| tree < &forest[x][y])
+        || view_l(forest, x, y).iter().all(|tree| tree < &forest[x][y])
+        || view_b(forest, x, y).iter().all(|tree| tree < &forest[x][y])
+        || view_r(forest, x, y).iter().all(|tree| tree < &forest[x][y])
 }
 
 fn scenic_score(forest: &[Vec<char>], x: usize, y: usize) -> usize
 {
-    let score_t = view_score(forest, view_t(forest, x, y), x, y);
-    let score_l = view_score(forest, view_l(forest, x, y), x, y);
-    let score_b = view_score(forest, view_b(forest, x, y), x, y);
-    let score_r = view_score(forest, view_r(forest, x, y), x, y);
-
-    score_t * score_l * score_b * score_r
+    view_score(forest, view_t(forest, x, y), x, y)
+        * view_score(forest, view_l(forest, x, y), x, y)
+        * view_score(forest, view_b(forest, x, y), x, y)
+        * view_score(forest, view_r(forest, x, y), x, y)
 }
 
 fn view_score(forest: &[Vec<char>], view: Vec<char>, x: usize, y: usize) -> usize
@@ -101,3 +73,29 @@ mod tests
         assert_eq!(super::solve(), (1845, 230112));
     }
 }
+
+// Mutithreaded solution:
+
+// let mut result1 = std::sync::Mutex::new(0);
+// let mut result2 = std::sync::Mutex::new(0);
+
+// std::thread::scope(|scope| {
+//     let input = &input; // shadowing to enable move only for x
+//     let result1 = &result1;
+//     let result2 = &result2;
+
+//     for x in 0..input.len() {
+//         scope.spawn(move || {
+//             for y in 0..input[0].len() {
+//                 let mut result1 = result1.lock().unwrap();
+//                 let mut result2 = result2.lock().unwrap();
+
+//                 *result1 += visible_from_outside(input, x, y) as usize;
+//                 *result2 = (*result2).max(scenic_score(input, x, y));
+//             }
+//         });
+//     }
+// });
+
+// let result1 = result1.lock().unwrap().to_owned();
+// let result2 = result2.lock().unwrap().to_owned();
