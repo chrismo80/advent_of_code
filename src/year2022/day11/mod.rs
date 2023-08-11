7acb3273d4d539f1907a454b834b675fe5740977
chrismo80 <krizm0@freenet.de>
2023-08-11 17:36:10 +0200

    Final day 11

--- src/year2022/day11/mod.rs
+++ src/year2022/day11/mod.rs
@@ -1,20 +1,17 @@
-use fasteval::*;
-use std::collections::*;
-
 #[derive(Clone, Debug)]
 struct Monkey
 {
     items: Vec<i64>,
     operation: String,
     test: i64,
-    throw_true: i32,
-    throw_false: i32,
-    inspections: i64,
+    throw_true: usize,
+    throw_false: usize,
+    inspections: usize,
 }
 
-pub fn solve() -> (i64, i64)
+pub fn solve() -> (usize, usize)
 {
-    let input = include_str!("test.txt")
+    let input = include_str!("input.txt")
         .split("\n\n")
         .map(|monkey| {
             monkey
@@ -43,26 +40,23 @@ pub fn solve() -> (i64, i64)
         })
         .collect::<Vec<Vec<String>>>();
 
-    let mut monkeys = HashMap::<i32, Monkey>::new();
+    let mut monkeys: Vec<Monkey> = Vec::new();
 
     for chunk in input {
-        monkeys.insert(
-            chunk[0].parse::<i32>().unwrap(),
-            Monkey {
-                items: chunk[1]
-                    .split(", ")
-                    .map(|i| i.parse::<i64>().unwrap())
-                    .collect::<Vec<i64>>(),
-                operation: chunk[2].clone(),
-                test: chunk[3].parse::<i64>().unwrap(),
-                throw_true: chunk[4].parse::<i32>().unwrap(),
-                throw_false: chunk[5].parse::<i32>().unwrap(),
-                inspections: 0,
-            },
-        );
+        monkeys.push(Monkey {
+            items: chunk[1]
+                .split(", ")
+                .map(|i| i.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>(),
+            operation: chunk[2].clone(),
+            test: chunk[3].parse::<i64>().unwrap(),
+            throw_true: chunk[4].parse::<usize>().unwrap(),
+            throw_false: chunk[5].parse::<usize>().unwrap(),
+            inspections: 0,
+        });
     }
 
-    let lcm: i64 = monkeys.values().map(|monkey| monkey.test).product();
+    let lcm: i64 = monkeys.iter().map(|monkey| monkey.test).product();
 
     let result1 = play(monkeys.clone(), lcm, 20);
     let result2 = play(monkeys.clone(), lcm, 10_000);
@@ -72,56 +66,49 @@ pub fn solve() -> (i64, i64)
     (result1, result2)
 }
 
-fn play(mut monkeys: HashMap<i32, Monkey>, lcm: i64, rounds: i32) -> i64
+fn play(mut monkeys: Vec<Monkey>, lcm: i64, rounds: i32) -> usize
 {
     let mut ns = fasteval::EmptyNamespace;
 
     for _ in 0..rounds {
         for i in 0..monkeys.len() {
-            let mut items_true: (i32, Vec<i64>) = (-1, Vec::new());
-            let mut items_false: (i32, Vec<i64>) = (-1, Vec::new());
+            let monkey = &mut monkeys[i];
 
-            {
-                let monkey = monkeys.get_mut(&(i as i32)).unwrap();
+            monkey.inspections += monkey.items.len();
 
-                monkey.inspections += monkey.items.len() as i64;
+            let mut items_true = vec![];
+            let mut items_false = vec![];
 
-                items_true.0 = monkey.throw_true;
-                items_false.0 = monkey.throw_false;
+            while let Some(item) = monkey.items.pop() {
+                let mut worry_level =
+                    fasteval::ez_eval(&monkey.operation.replace("old", item.to_string().as_str()), &mut ns).unwrap()
+                        as i64;
 
-                while let Some(item) = monkey.items.pop() {
-                    let mut worry_level =
-                        fasteval::ez_eval(&monkey.operation.replace("old", format!("{}", item).as_str()), &mut ns)
-                            .unwrap() as i64;
+                if rounds < 1000 {
+                    worry_level /= 3;
+                }
 
-                    if rounds < 1000 {
-                        worry_level /= 3;
-                    }
-
-                    if worry_level % monkey.test == 0 {
-                        items_true.1.push(worry_level % lcm);
-                    }
-                    else {
-                        items_false.1.push(worry_level % lcm);
-                    }
+                if worry_level % monkey.test == 0 {
+                    items_true.push(worry_level % lcm);
+                }
+                else {
+                    items_false.push(worry_level % lcm);
                 }
             }
 
-            for item in items_true.1 {
-                monkeys.get_mut(&items_true.0).unwrap().items.push(item);
-            }
+            let monkey_true = monkey.throw_true;
+            let monkey_false = monkey.throw_false;
 
-            for item in items_false.1 {
-                monkeys.get_mut(&items_false.0).unwrap().items.push(item);
-            }
+            monkeys[monkey_true].items.extend(items_true);
+            monkeys[monkey_false].items.extend(items_false);
         }
     }
 
-    let mut result: Vec<i64> = monkeys.values().map(|monkey| monkey.inspections).collect();
+    let mut result: Vec<usize> = monkeys.iter().map(|monkey| monkey.inspections).collect();
 
     result.sort();
 
-    result.iter().rev().take(2).product::<i64>()
+    result.iter().rev().take(2).product::<usize>()
 }
 
 #[cfg(test)]
@@ -130,8 +117,6 @@ mod tests
     #[test]
     fn solve()
     {
-        //assert_eq!(super::solve(), (10605, 2713310158)); // 10197
-
-        assert_eq!(super::solve(), (100345, 28537348205)); // 105210
+        assert_eq!(super::solve(), (100345, 28537348205));
     }
 }
