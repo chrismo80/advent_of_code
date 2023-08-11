a6837c1b5a2082535942f46150a26bec91d78607
chrismo80 <krizm0@freenet.de>
2023-08-11 21:39:38 +0200

    add BSF on a grid for day 12

--- src/year2022/day12/mod.rs
+++ src/year2022/day12/mod.rs
@@ -1,8 +1,31 @@
 pub fn solve() -> (usize, usize)
 {
-    let input = include_str!("input.txt");
+    let mut map = include_str!("input.txt")
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
 
-    let result1 = 0;
+    let mut start = Coord(0, 0);
+    let mut end = Coord(0, 0);
+
+    for row in 0..map.len() {
+        for col in 0..map[0].len() {
+            if map[row][col] == 'S' {
+                start = Coord(col, row);
+                map[row][col] = 'a';
+            }
+
+            if map[row][col] == 'E' {
+                end = Coord(col, row);
+                map[row][col] = 'z';
+            }
+        }
+    }
+
+    let walkable = Box::new(|current: &char, neighbor: &char| *neighbor as i32 - *current as i32 <= 1);
+    let grid = Grid::new(map, walkable);
+
+    let result1 = grid.bfs(start, end).unwrap().len() - 1;
     let result2 = 0;
 
     println!("12\t{result1}\t{result2}");
@@ -17,5 +40,76 @@ mod tests
     fn solve()
     {
         assert_eq!(super::solve(), (0, 0));
+    }
+}
+
+use std::collections::*;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+struct Coord(usize, usize);
+
+struct Grid<T>
+{
+    map: Vec<Vec<T>>,
+    walkable: Box<dyn Fn(&T, &T) -> bool>,
+}
+
+impl<T> Grid<T>
+where
+    T: std::fmt::Debug,
+{
+    fn new(map: Vec<Vec<T>>, walkable: Box<dyn Fn(&T, &T) -> bool>) -> Self
+    {
+        Grid { map, walkable }
+    }
+
+    fn bfs(&self, start: Coord, end: Coord) -> Option<Vec<Coord>>
+    {
+        let mut queue = VecDeque::new();
+        queue.push_back(vec![start]);
+        let mut visited = HashMap::new();
+        visited.insert(start, start);
+
+        while let Some(path) = queue.pop_front() {
+            let current = path.last().unwrap();
+
+            if *current == end {
+                return Some(path);
+            }
+
+            for neighbor in self.neighbors(*current) {
+                if !visited.contains_key(&neighbor)
+                    && (self.walkable)(&self.map[current.1][current.0], &self.map[neighbor.1][neighbor.0])
+                {
+                    visited.insert(neighbor, *current);
+                    let mut new_path = path.clone();
+                    new_path.push(neighbor);
+                    queue.push_back(new_path);
+                }
+            }
+        }
+
+        None
+    }
+
+    fn neighbors(&self, coord: Coord) -> Vec<Coord>
+    {
+        let mut neighbors = Vec::new();
+        let (x, y) = (coord.0 as isize, coord.1 as isize);
+
+        if y > 0 {
+            neighbors.push(Coord(x as usize, (y - 1) as usize));
+        }
+        if x > 0 {
+            neighbors.push(Coord((x - 1) as usize, y as usize));
+        }
+        if y < self.map.len() as isize - 1 {
+            neighbors.push(Coord(x as usize, (y + 1) as usize));
+        }
+        if x < self.map[0].len() as isize - 1 {
+            neighbors.push(Coord((x + 1) as usize, y as usize));
+        }
+
+        neighbors
     }
 }
