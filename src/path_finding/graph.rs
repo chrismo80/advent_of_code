88bbe3450355b879bf35fc11dc85d86e8673dddf
chrismo80 <krizm0@freenet.de>
2023-08-21 07:41:21 +0200

    prepare graph

--- src/path_finding/graph.rs
+++ src/path_finding/graph.rs
@@ -1,1 +1,10 @@
+use std::collections::*;
 
+#[derive(Default)]
+pub struct Graph<T>
+{
+    map: HashMap<T, HashMap<T, i32>>,
+    previous: HashMap<T, T>,
+}
+
+impl<T> Graph<T> where T: Clone + std::fmt::Debug {}
