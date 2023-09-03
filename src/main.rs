1ac271de1caaa6df6baca65c902e7034637f82a7
chrismo80 <krizm0@freenet.de>
2023-09-03 12:04:51 +0200

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,5 @@ fn main()
     // enable callstack for panics, must be set before the panic occurs
     //std::env::set_var("RUST_BACKTRACE", "1");
 
-    //advent_of_code::examples::http_request::main();
-
     advent_of_code::run();
 }
