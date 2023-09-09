5330d23b9471c12eca3165ea58de34f0dae57055
chrismo80 <krizm0@freenet.de>
2023-09-09 18:26:58 +0200

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -42,12 +42,7 @@ fn end(start: std::time::Instant, scope: &str)
 {
     print_line();
 
-    if scope == "Total" {
-        println!("{scope}: {:.1} s\n\n", start.elapsed().as_millis() as f32 / 1000.0);
-        return;
-    }
-
-    println!("{scope}: {:.0} ms\n\n", start.elapsed().as_micros() as f32 / 1000.0);
+    println!("{scope}: {:.1} ms\n\n", start.elapsed().as_micros() as f32 / 1000.0);
 }
 
 fn print_line()
