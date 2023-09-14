7839afeb0bdabb3e8eb09408d4addb3450414334
chrismo80 <krizm0@freenet.de>
2023-09-14 20:19:25 +0200

    2016 / 15

---
+++ src/year2016/day15/mod.rs
@@ -0,0 +1,34 @@
+pub fn solve() -> (usize, usize)
+{
+    let mut discs: Vec<Vec<usize>> = include_str!("input.txt")
+        .lines()
+        .map(|line| {
+            line.split([' ', '#', '.'].as_ref())
+                .filter_map(|word| word.parse().ok())
+                .collect::<Vec<usize>>()
+        })
+        .collect();
+
+    let result1 = find_wait_time(&discs);
+
+    discs.push(vec![discs.len() + 1, 11, 0]);
+
+    let result2 = find_wait_time(&discs);
+
+    println!("15\t{result1:<20}\t{result2:<20}");
+
+    (result1, result2)
+}
+
+fn find_wait_time(discs: &[Vec<usize>]) -> usize
+{
+    (0..usize::MAX)
+        .find(|&delay| discs.iter().all(|d| (d[0] + d[2] + delay) % d[1] == 0))
+        .unwrap()
+}
+
+#[test]
+fn test()
+{
+    assert_eq!(solve(), (122318, 3208583));
+}
--- src/year2016/mod.rs
+++ src/year2016/mod.rs
@@ -3,6 +3,7 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day13;
+pub mod day15;
 pub mod day16;
 pub mod day20;
 pub mod day24;
@@ -18,6 +19,7 @@ pub fn run_all()
     day03::solve();
     day04::solve();
     day13::solve();
+    day15::solve();
     day16::solve();
     day20::solve();
     day24::solve();
