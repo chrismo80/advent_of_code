d0ffc132e200805d9d639813224e5b0dc3850f6c
chrismo80 <krizm0@freenet.de>
2023-09-14 17:00:46 +0200

    2017 / 19

--- src/year2017/day19/mod.rs
+++ src/year2017/day19/mod.rs
@@ -14,23 +14,12 @@ pub fn solve() -> (String, usize)
         steps += 1;
 
         match map[y][x] {
-            '+' => match face % 2 {
-                0 => {
-                    if x > 0 && map[y][x - 1] != ' ' {
-                        face = 3;
-                    }
-                    else {
-                        face = 1;
-                    }
-                }
-                _ => {
-                    if y > 0 && map[y - 1][x] != ' ' {
-                        face = 0;
-                    }
-                    else {
-                        face = 2;
-                    }
-                }
+            '+' => match (face % 2, x > 0 && map[y][x - 1] != ' ', y > 0 && map[y - 1][x] != ' ') {
+                (0, true, _) => face = 3,
+                (0, false, _) => face = 1,
+                (1, _, true) => face = 0,
+                (1, _, false) => face = 2,
+                _ => unreachable!(),
             },
             '|' | '-' => {}
             c => letters.push(c),
