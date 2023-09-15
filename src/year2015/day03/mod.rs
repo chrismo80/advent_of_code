6d84d69be4eb8f3d081fb8115a15633d0caa4b77
chrismo80 <krizm0@freenet.de>
2023-09-15 13:43:49 +0200

    2015 / 03

--- src/year2015/day03/mod.rs
+++ src/year2015/day03/mod.rs
@@ -7,17 +7,17 @@ pub fn solve() -> (usize, usize)
     let mut houses1 = vec![(0, 0)];
     let mut houses2 = vec![(0, 0)];
 
-    for &step in input.iter() {
+    for step in input.iter() {
         houses1.push(new_house(houses1.last().unwrap(), step));
     }
 
-    for &step in input.iter().step_by(2) {
+    for step in input.iter().step_by(2) {
         houses2.push(new_house(houses2.last().unwrap(), step));
     }
 
     houses2.push((0, 0));
 
-    for &step in input.iter().skip(1).step_by(2) {
+    for step in input.iter().skip(1).step_by(2) {
         houses2.push(new_house(houses2.last().unwrap(), step));
     }
 
@@ -29,7 +29,7 @@ pub fn solve() -> (usize, usize)
     (result1, result2)
 }
 
-fn new_house(last_house: &(i32, i32), direction: char) -> (i32, i32)
+fn new_house(last_house: &(i32, i32), direction: &char) -> (i32, i32)
 {
     match direction {
         '>' => (last_house.0, last_house.1 + 1),
