8aa1ade275a44253a9271811ee0ab06db37fc10f
chrismo80 <krizm0@freenet.de>
2023-09-15 11:22:48 +0200

    2017 / 12

--- src/year2017/day12/mod.rs
+++ src/year2017/day12/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 pub fn solve() -> (usize, usize)
 {
     let input: Vec<Vec<usize>> = include_str!("input.txt")
@@ -7,43 +5,28 @@ pub fn solve() -> (usize, usize)
         .map(|l| l.split([' ', ',']).filter_map(|e| e.parse().ok()).collect())
         .collect();
 
-    let all: HashSet<usize> = HashSet::from_iter(input.iter().flatten().cloned());
-    let mut found: HashSet<usize> = HashSet::new();
+    let mut done = vec![false; input.len()];
+    let mut group = Vec::new();
+    let mut groups = Vec::new();
+    let mut count;
 
-    let mut count = 0;
-    let mut groups = 0;
-    let mut result1 = 0;
+    while done.iter().any(|p| !p) {
+        count = 0;
+        group.push(done.iter().position(|p| !p).unwrap());
 
-    while found.len() < all.len() {
-        let start = match found.len() {
-            0 => 0,
-            _ => *all.difference(&found).next().unwrap(),
-        };
-
-        found.insert(start);
-
-        while count != found.len() {
-            count = found.len();
-
-            let to_search: Vec<&Vec<usize>> = input.iter().filter(|&p| !found.contains(p.first().unwrap())).collect();
-
-            for connections in to_search.iter() {
-                let h = HashSet::from_iter(connections.iter().copied());
-
-                if found.intersection(&h).count() > 0 {
-                    found = found.union(&h).cloned().collect();
-                }
+        while let Some(i) = group.pop() {
+            if !done[i] {
+                done[i] = true;
+                count += 1;
+                group.extend(input[i].iter().filter(|&p| !done[*p]));
             }
         }
 
-        groups += 1;
-
-        if result1 == 0 {
-            result1 = found.len();
-        }
+        groups.push(count);
     }
 
-    let result2 = groups;
+    let result1 = *groups.first().unwrap();
+    let result2 = groups.len();
 
     println!("12\t{result1:<20}\t{result2:<20}");
 
