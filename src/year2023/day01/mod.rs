deec1d07312f072f193089068ad4cbcdc7c0f673
chrismo80 <krizm0@freenet.de>
2023-12-03 19:04:03 +0100

    2023 / 01

--- src/year2023/day01/mod.rs
+++ src/year2023/day01/mod.rs
@@ -1,23 +1,11 @@
-use std::collections::HashMap;
-
 pub fn solve() -> (i64, i64)
 {
     let input: Vec<&str> = include_str!("input.txt").lines().collect();
 
-    let map = HashMap::from([
-        ("one", "o1e"),
-        ("two", "t2o"),
-        ("three", "t3e"),
-        ("four", "f4r"),
-        ("five", "f5e"),
-        ("six", "s6x"),
-        ("seven", "s7n"),
-        ("eight", "e8t"),
-        ("nine", "n9e"),
-    ]);
+    let names = vec!["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
     let result1 = input.iter().map(|line| get_calibration_value(line)).sum::<i64>();
-    let result2 = input.iter().map(|line| get_real_calibration_value(line, &map)).sum::<i64>();
+    let result2 = input.iter().map(|line| get_real_calibration_value(line, &names)).sum::<i64>();
 
     println!("1\t{result1:<20}\t{result2:<20}");
 
@@ -33,12 +21,12 @@ fn get_calibration_value(text: &str) -> i64
         .unwrap()
 }
 
-fn get_real_calibration_value(text: &str, map: &HashMap<&str, &str>) -> i64
+fn get_real_calibration_value(text: &str, names: &[&str]) -> i64
 {
     let mut replaced = text.to_owned();
 
-    for (key, value) in map.iter() {
-        replaced = replaced.replace(key, value);
+    for (i, name) in names.iter().enumerate() {
+        replaced = replaced.replace(name, format!("{}{}{}", name, i, name).as_str());
     }
 
     get_calibration_value(replaced.as_str())
