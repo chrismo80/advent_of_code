ed3710bdd74dac7a55c9354d418f8f44ecfb8762
chrismo80 <krizm0@freenet.de>
2023-12-04 21:50:03 +0100

    2023 / 04

--- src/year2023/day04/mod.rs
+++ src/year2023/day04/mod.rs
@@ -2,19 +2,19 @@ use crate::extensions::converter::Parser;
 
 pub fn solve() -> (usize, usize)
 {
-    let cards = include_str!("input.txt").to_vec::<Card>("\n");
+    let matches: Vec<usize> = include_str!("input.txt").lines().map(get_matches).collect();
 
-    let mut counters = vec![1; cards.len()];
+    let mut counters = vec![1; matches.len()];
 
     for i in 0..counters.len() {
-        for j in i..i + cards[i].matches {
+        for j in i..i + matches[i] {
             if j + 1 < counters.len() {
                 counters[j + 1] += counters[i];
             }
         }
     }
 
-    let result1 = cards.iter().map(|card| 2_usize.pow(card.matches as u32) / 2).sum();
+    let result1 = matches.iter().map(|&m| 2_usize.pow(m as u32) / 2).sum();
     let result2 = counters.iter().sum();
 
     println!("4\t{result1:<20}\t{result2:<20}");
@@ -22,26 +22,13 @@ pub fn solve() -> (usize, usize)
     (result1, result2)
 }
 
-impl std::str::FromStr for Card
+fn get_matches(card: &str) -> usize
 {
-    type Err = ();
+    let mut sets = card.split(':').next_back().unwrap().split('|');
+    let wins = sets.next().unwrap().to_vec::<usize>(" ");
+    let hand = sets.next().unwrap().to_vec::<usize>(" ");
 
-    fn from_str(s: &str) -> Result<Self, Self::Err>
-    {
-        let mut sets = s.split(':').next_back().unwrap().split('|');
-        let wins = sets.next().unwrap().to_vec::<usize>(" ");
-        let hand = sets.next().unwrap().to_vec::<usize>(" ");
-        let matches = hand.iter().filter(|&n| wins.contains(n)).count();
-
-        Ok(Card { wins, hand, matches })
-    }
-}
-
-struct Card
-{
-    wins: Vec<usize>,
-    hand: Vec<usize>,
-    matches: usize,
+    hand.iter().filter(|&n| wins.contains(n)).count()
 }
 
 #[test]
