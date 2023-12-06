72e96cbd29183cf8e4887f02fb130eb963e1ffc8
chrismo80 <krizm0@freenet.de>
2023-12-06 23:08:50 +0100

    2023 / 06

--- src/year2023/day06/mod.rs
+++ src/year2023/day06/mod.rs
@@ -15,16 +15,16 @@ pub fn solve() -> (usize, usize)
     (result1, result2)
 }
 
-fn merge(v: Vec<usize>) -> usize
-{
-    v.iter().map(|x| x.to_string()).collect::<String>().parse::<usize>().unwrap()
-}
-
 fn count(time: usize, record: usize) -> usize
 {
     let last = (time / 2..time).find(|speed| speed * (time - speed) < record).unwrap();
 
     2 * last - time - 1
+}
+
+fn merge(v: Vec<usize>) -> usize
+{
+    v.iter().map(|x| x.to_string()).collect::<String>().parse::<usize>().unwrap()
 }
 
 #[test]
