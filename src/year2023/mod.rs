d850a34da2713f2395013d47c9ab4eb7c87cb850
chrismo80 <krizm0@freenet.de>
2023-12-10 16:07:18 +0100

    revert run all days from 2023

--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -15,15 +15,15 @@ pub fn run_all()
 {
     let start = super::begin();
 
-    // day01::solve();
-    // day02::solve();
-    // day03::solve();
-    // day04::solve();
-    // day05::solve();
-    // day06::solve();
-    // day07::solve();
-    // day08::solve();
-    // day09::solve();
+    day01::solve();
+    day02::solve();
+    day03::solve();
+    day04::solve();
+    day05::solve();
+    day06::solve();
+    day07::solve();
+    day08::solve();
+    day09::solve();
     day10::solve();
 
     super::end(start, YEAR);
